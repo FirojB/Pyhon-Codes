276f6b91e8a450b2b6292cd0a71d547b0a4efe1f
Firoj Ahmad Bakhshi <62630180+FirojB@users.noreply.github.com>
2021-07-20 04:17:51 +0100

    Add files via upload
    # Random Password Generator Project
    import random
    
    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
               'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
               'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
    
    print("Welcome to the PyPassword Generator!")
    nr_letters = int(input("How many letters would you like in your password?\n"))
    nr_symbols = int(input(f"How many symbols would you like?\n"))
    nr_numbers = int(input(f"How many numbers would you like?\n"))
    
    pass1 = [random.choice(letters) for num in range(0, nr_letters)]
    pass2 = [random.choice(symbols) for num in range(0, nr_symbols)]
    pass3 = [random.choice(numbers) for num in range(0, nr_numbers)]
    
    passwrod = pass1 + pass2 + pass3
    random.shuffle(passwrod)
    
    # List to sentance conversion
    suffled = ''.join(passwrod)
    print("The password is : ", suffled)

---
+++ Project 5e.py
@@ -0,0 +1,24 @@
+# Random Password Generator Project
+import random
+
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
+           'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
+           'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+print("Welcome to the PyPassword Generator!")
+nr_letters = int(input("How many letters would you like in your password?\n"))
+nr_symbols = int(input(f"How many symbols would you like?\n"))
+nr_numbers = int(input(f"How many numbers would you like?\n"))
+
+pass1 = [random.choice(letters) for num in range(0, nr_letters)]
+pass2 = [random.choice(symbols) for num in range(0, nr_symbols)]
+pass3 = [random.choice(numbers) for num in range(0, nr_numbers)]
+
+passwrod = pass1 + pass2 + pass3
+random.shuffle(passwrod)
+
+# List to sentance conversion
+suffled = ''.join(passwrod)
+print("The password is : ", suffled)
