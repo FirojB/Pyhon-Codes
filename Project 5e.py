1f3e362a3694b59e2f6669b34d9719834dc1ad22
Firoj Ahmad Bakhshi <62630180+FirojB@users.noreply.github.com>
2021-07-20 04:20:04 +0100

    Update Project 5e.py

--- Project 5e.py
+++ Project 5e.py
@@ -8,9 +8,9 @@ numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
 print("Welcome to the PyPassword Generator!")
-nr_letters = int(input("How many letters would you like in your password?\n"))
-nr_symbols = int(input(f"How many symbols would you like?\n"))
-nr_numbers = int(input(f"How many numbers would you like?\n"))
+nr_letters = int(input("How many letters would you like in your password? : "))
+nr_symbols = int(input(f"How many symbols would you like? : "))
+nr_numbers = int(input(f"How many numbers would you like? : "))
 
 pass1 = [random.choice(letters) for num in range(0, nr_letters)]
 pass2 = [random.choice(symbols) for num in range(0, nr_symbols)]
