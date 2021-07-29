47425e49ae33dd8f82aa9305f08fdcbc660e8bb9
unknown <zf18aaq@herts.ac.uk>
2021-07-29 01:50:31 +0100

    Random Password Generator Project

--- Project 5e.py
+++ Project 5e.py
@@ -1,6 +1,4 @@
-
 # Random Password Generator Project
-
 import random
 
 letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
@@ -10,9 +8,9 @@ numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
 print("Welcome to the PyPassword Generator!")
-nr_letters = int(input("How many letters would you like in your password? : "))
-nr_symbols = int(input(f"How many symbols would you like? : "))
-nr_numbers = int(input(f"How many numbers would you like? : "))
+nr_letters = int(input("How many letters would you like in your password?\n"))
+nr_symbols = int(input(f"How many symbols would you like?\n"))
+nr_numbers = int(input(f"How many numbers would you like?\n"))
 
 pass1 = [random.choice(letters) for num in range(0, nr_letters)]
 pass2 = [random.choice(symbols) for num in range(0, nr_symbols)]
@@ -23,5 +21,4 @@ random.shuffle(passwrod)
 
 # List to sentance conversion
 suffled = ''.join(passwrod)
-
 print("The password is : ", suffled)
